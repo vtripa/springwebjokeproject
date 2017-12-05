d008ac3552271127762a50f05d4e644743f3a517
129472 <129472@INDSNOIDV3K622.AMERICAS.GLOBAL.NTTDATA.COM>
2017-12-05 17:42:46 +0530

    Initial Spring joke web project

---
+++ springjoke/src/main/java/com/vt/spring/joke/SpringjokeApplication.java
@@ -0,0 +1,12 @@
+package com.vt.spring.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjokeApplication.class, args);
+	}
+}
---
+++ springjoke/src/test/java/com/vt/spring/joke/SpringjokeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vt.spring.joke;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringjokeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
