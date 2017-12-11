8ef00f0e1f792f5bc854f229adc96b15a7b5bfe3
129472 <129472@INDSNOIDV3K622.AMERICAS.GLOBAL.NTTDATA.COM>
2017-12-11 13:00:06 +0530

    updated code

--- springjoke/src/main/java/com/vt/spring/joke/SpringjokeApplication.java
+++ springjoke/src/main/java/com/vt/spring/joke/SpringjokeApplication.java
@@ -2,8 +2,10 @@ package com.vt.spring.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource ("classpath:spring-config.xml")
 public class SpringjokeApplication {
 
 	public static void main(String[] args) {
--- springjoke/src/main/java/com/vt/spring/joke/services/JokeServiceImpl.java
+++ springjoke/src/main/java/com/vt/spring/joke/services/JokeServiceImpl.java
@@ -9,12 +9,11 @@ public class JokeServiceImpl implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
 	
-	public JokeServiceImpl() {
-		chuckNorrisQuotes = new ChuckNorrisQuotes();
+	public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}
 	@Override
 	public String getJoke() {
 		return chuckNorrisQuotes.getRandomQuote();
 	}
-
 }
